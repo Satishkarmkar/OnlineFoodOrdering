23ba00b9ff4462d7248f5d8c06de8a0e0e1b3dd0
Satish Kumar Karmkar <satishkarmkar031@gmail.com>
2020-09-08 21:50:51 +0530

    openstate
    openstate page finished

--- pwn/urls.py
+++ pwn/urls.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 from django.urls import path
 from pwn import views
@@ -5,5 +6,22 @@ from pwn import views
 urlpatterns = [
     path('',views.showIndex,name='index'),
     path('pwn_login_check/',views.pwn_login_check,name="pwn_login_check"),
-    path('welcome/',views.welcome,name="welcome")
+    path('welcome/',views.welcome,name="welcome"),
+
+    path('state/',views.openState,name="state"),
+    path('savestate/',views.saveState,name="savestate"),
+    path('updatestate/',views.updateState,name="updatestate"),
+    path('updatestateid/',views.updateStateid,name="updatestateid"),
+    path('deletestate/',views.deleteState,name="deletestate"),
+
+
+
+
+    path('city/',views.openCity,name="city"),
+    path('cuisine/',views.openCuisine,name="cuisine"),
+    path('vendor/',views.openVendor,name="vendor"),
+    path('reports/',views.openReports,name="reports"),
+    path('logout/',views.pwn_login_check,name="logout"),
+
+
 ]
--- pwn/views.py
+++ pwn/views.py
@@ -1,18 +1,67 @@
 from django.shortcuts import render,redirect
-from pwn.models import AdminLoginModel
+from pwn.models import AdminLoginModel,StateModel
+from django.contrib import messages
 # Create your views here.
 
 def showIndex(request):
     return render(request,"pwn/login.html")
+
+
 def pwn_login_check(request):
+    if request.method == "POST":
         try:
             admin = AdminLoginModel.objects.get(username=request.POST.get("pwn_username"),password=request.POST.get("pwn_password"))
             request.session["admin_status"]=True
             return redirect('welcome')
         except:
             return render(request,"pwn/login.html",{"error":"Invalid Credentials"})
+    else:
+        request.session["admin_status"]=False
+        return render(request,"pwn/login.html",{"error":"Admin Logout Successfully"})
+
 
 def welcome(request):
-    request.session["admin_status"] = True
-    return render(request,"pwn/welcome.html")
+    return render(request,"pwn/home.html")
+
+
+def openState(request):
+    statedetails = StateModel.objects.all()
+    return render(request,"pwn/openstate.html",{"sd":statedetails})
+
+def saveState(request):
+    statename = request.POST.get('statename')
+    statephoto = request.FILES['stateimage']
+    StateModel(name=statename,photo=statephoto).save()
+    messages.success(request,"State Saved Successfully")
+    return openState(request)
+
+def updateState(request):
+    sid = request.GET.get('id')
+    sm = StateModel.objects.get(id=sid)
+    return render(request,'pwn/openstate.html',{'udata':sm})
+
+def updateStateid(request):
+    StateModel.objects.filter(id=request.GET.get('sid')).update(name=request.POST.get('statename'),photo=request.FILES.get('stateimage'))
+    return redirect('state')
+
+def deleteState(request):
+    StateModel.objects.filter(id=request.GET.get('sid')).delete()
+    messages.success(request,'state deleted')
+    return redirect('state')
+
+
+def openCity(request):
+    return render(request,"pwn/opencity.html")
+
+
+def openCuisine(request):
+    return render(request,"pwn/opencuisine.html")
+
+
+def openVendor(request):
+    return render(request,"pwn/openvendor.html")
+
+
+def openReports(request):
+    return render(request,"pwn/openreports.html")
 
